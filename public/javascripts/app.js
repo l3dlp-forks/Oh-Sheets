9c2bfa3eda489d2b01ebd5d1aefe6d3691a981f8
Brian <brian.oley@gmail.com>
2012-01-10 22:17:36 -0500

    switched to backbone, built models

---
+++ public/javascripts/app.js
@@ -0,0 +1,47 @@
+$(function(){
+    //Models
+    window.Timesheet = Backbone.Model.extend({
+        initialize: function(){
+            this.days = new SheetDays;
+        }
+    });
+
+    window.SheetDays = Backbone.Collection.extend({
+        model:SheetDay
+    });
+
+    window.SheetDay =  Backbone.Model.extend({
+        initialize: function() {
+            this.activities = new SheetActivities;
+        }
+    });
+
+    window.SheetActivities = Backbone.Collection.extend({
+        model: SheetActivity
+    });
+
+    window.SheetActivity = Backbone.Model.extend({
+        defaults: function(){
+            return {
+                timeStart: "9:00am",
+                timeEnd: "5:00pm",
+                projectName: "Metagame",
+                projectVersion: "1.0",
+                projectPlatform: "iPhone",
+                activityName: "Working"
+            };
+        }
+
+    });
+
+    window.WorkingTimesheet = new Timesheet;
+
+    //Views
+    window.ActivityView = Backbone.View.extend({
+        tagName: "li",
+
+        template: _.template($('#activity-template').html()),
+
+    });
+
+})();
