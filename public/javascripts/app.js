ff450242f3862f02a8037d52d917f1c40fd317fb
Brian Oley <brian.oley@gmail.com>
2012-02-08 22:30:39 -0500

    added template code, restarting app code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,47 +1,15 @@
-$(function(){
-    //Models
+(function($) {
     window.Timesheet = Backbone.Model.extend({
-        initialize: function(){
-            this.days = new SheetDays;
-        }
+        
     });
 
-    window.SheetDays = Backbone.Collection.extend({
-        model:SheetDay
-    });
-
-    window.SheetDay =  Backbone.Model.extend({
-        initialize: function() {
-            this.activities = new SheetActivities;
-        }
-    });
-
-    window.SheetActivities = Backbone.Collection.extend({
-        model: SheetActivity
-    });
-
-    window.SheetActivity = Backbone.Model.extend({
-        defaults: function(){
-            return {
-                timeStart: "9:00am",
-                timeEnd: "5:00pm",
-                projectName: "",
-                projectVersion: "1.0",
-                projectPlatform: "iPhone",
-                activityName: ""
-            };
-        }
+    window.TimesheetView = Backbone.View.extend({
 
     });
 
-    window.WorkingTimesheet = new Timesheet;
-
-    //Views
-    window.ActivityView = Backbone.View.extend({
-        tagName: "li",
-
-        template: _.template($('#activity-template').html()),
-
+    window.TimesheetCollection = Backbone.Collection.extend({
+        model: Timesheet,
+        url: "/timesheets"
     });
 
-})();
+})(jQuery);
